f3c9ce48dc2f1b991a6b8f4095ff78c900787595
Emanuele Mazza <n3m3da@d3cod3.org>
2016-05-30 15:08:24 +0200

    First Commit

---
+++ spec/gamuza-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GamuzaAtomView from '../lib/gamuza-atom-view';
+
+describe('GamuzaAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
